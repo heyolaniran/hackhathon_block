f5c6ecd6f9719197acbc1604ab8d875c915e2811
olaniranlaurent <olaniranlaurent@gmail.com>
2024-10-23 11:03:58 +0100

    feat: integrating convex and configurations

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, omnis pariatur. Dolore molestias architecto, vero harum veniam est non dolorem ipsa rerum, iusto iure? Dolor ab illum ducimus sunt vel?</p>
+        </div>
+    )
+}
